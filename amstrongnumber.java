871ab51757b96c7ce4dbe48dae927a064a3a6b7a
Yogi2511 <103985143+AF0181988@users.noreply.github.com>
2022-05-12 22:31:07 -0700

    assignment

---
+++ amstrongnumber.java
@@ -0,0 +1,18 @@
+class amstrongnumber
+{
+  public static void main(String args[])
+  {
+    int number=345,n,temp,sum=0; 
+    n=number;
+    while(n>0)
+    {
+      temp=n%10;
+      sum=sum+(temp*temp*temp);
+      n=n/10;
+    }
+  if(sum==n)
+  System.out.println(number+" is an amstrong number");
+  else
+  System.out.println(number+" is not an amstrong number");
+  }
+}
